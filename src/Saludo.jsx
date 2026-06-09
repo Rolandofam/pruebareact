ddd344ee56ac402a54ae58dcd8668bb195f23353
otakusoft <otaku_soft@hotmail.com>
2026-06-09 18:09:37 -0400

    Saludo
    Se modifico el saludo

--- src/Saludo.jsx
+++ src/Saludo.jsx
@@ -2,7 +2,7 @@ function Saludo() {
     return ( 
         <>
             <h1> Esta es Mi pagina</h1>
-            <h2>Soy Rolando</h2>
+            <h2>Soy Rolando Aruquipa</h2>
         </>
      );
 }
